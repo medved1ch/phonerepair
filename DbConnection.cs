18a1915491855e79e278649b3ed5ef8694d537df
TeRReLL2k17 TeRReLL2K17 <kirillyaroslavsky@gmail.com>
2022-05-17 10:28:03 +0300

    Добавьте файлы проекта.

---
+++ DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace phonerepair
+{
+    class DbConnection
+    {
+        public static string myConn = $@"Data Source=DocAcc.db;Version=3;";
+    }
+}
